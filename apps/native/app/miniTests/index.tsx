45cc4fa82eb6b3e8560f9429cc42631ccee657f5
LeeinUITk17 <22521276@gm.uit.edu.vn>
2024-11-25 16:13:05 +0700

    fix: custom minitest progess

--- apps/native/app/miniTests/index.tsx
+++ apps/native/app/miniTests/index.tsx
@@ -1,24 +1,103 @@
+import { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Animated, ScrollView } from 'react-native';
+import Modal from 'react-native-modal';
 import { useLocalSearchParams } from 'expo-router';
-import React from 'react';
-import { View, Text, ScrollView } from 'react-native';
-
 import { MiniTest } from '~/types/type';
 import { convertStringToObject } from '~/utils/convertStringToObject';
+import { images } from '~/utils/icon';
 
 const MiniTests = () => {
   const { data } = useLocalSearchParams();
   const parsedData = convertStringToObject(data as string) as MiniTest[];
 
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [userAnswer, setUserAnswer] = useState('');
+  const [score, setScore] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [showErrorModal, setShowErrorModal] = useState(false);
+
+  const handleSubmitAnswer = () => {
+    if (userAnswer.trim() === '') {
+      setShowErrorModal(true);
+      return;
+    }
+
+    const currentAnswer = parsedData[currentQuestionIndex].answer.trim();
+
+    if (userAnswer.toLowerCase() === currentAnswer.toLowerCase()) {
+      setScore(score + 1);
+    }
+
+    setUserAnswer('');
+
+    if (currentQuestionIndex < parsedData.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      setShowModal(true);
+    }
+  };
+
   return (
-    <ScrollView className="bg-gray-100 p-4">
-      {parsedData.map((item) => (
-        <View key={item.id} className="mb-6 rounded-lg  bg-white p-4 shadow-md">
-          <Text className="text-lg font-semibold text-gray-700">{item.question}</Text>
-          <View className="mt-2">
-            <Text className="text-base text-gray-600">Answer: {item.answer}</Text>
-          </View>
+    <ScrollView className="bg-[#f7fafc] p-6">
+      <View className="bg-white p-6 rounded-xl shadow-xl mb-6">
+        <Text className="text-3xl font-bold text-gray-800 mb-3">
+          Question {currentQuestionIndex + 1} / {parsedData.length}
+        </Text>
+        <Text className="text-xl text-gray-700 mb-4">
+          {parsedData[currentQuestionIndex].question}
+        </Text>
+        <TextInput
+          className="mt-4 mb-4 p-3 border-2 border-gray-300 rounded-xl text-lg"
+          placeholder="Your answer"
+          value={userAnswer}
+          onChangeText={setUserAnswer}
+        />
+        <TouchableOpacity
+          onPress={handleSubmitAnswer}
+          className="bg-green-500 px-6 py-3 rounded-xl items-center"
+        >
+          <Text className="text-white font-semibold text-lg">Submit Answer</Text>
+        </TouchableOpacity>
+      </View>
+      <Animated.View className="flex-1 justify-center items-center">
+        <Animated.Image source={images.capy} className="h-[350px] w-[250px]" />
+      </Animated.View>
+
+      <Modal
+        isVisible={showErrorModal}
+        onBackdropPress={() => setShowErrorModal(false)}
+        onBackButtonPress={() => setShowErrorModal(false)}
+        animationIn="pulse"
+        animationOut="fadeOut"
+        backdropColor="rgba(0, 0, 0, 0.5)"
+        backdropOpacity={0.5}
+        useNativeDriver
+      >
+        <View className="bg-white rounded-lg p-6 shadow-lg max-h-1/3 mx-4">
+          <Text className="text-2xl font-bold text-red-500 mb-4">Error</Text>
+          <Text className="text-lg text-gray-700">
+            Please enter an answer before submitting.
+          </Text>
         </View>
-      ))}
+      </Modal>
+      <Modal
+        isVisible={showModal}
+        onBackdropPress={() => setShowModal(false)}
+        onBackButtonPress={() => setShowModal(false)}
+        animationIn="fadeInDown"
+        animationOut="fadeOutUp"
+        useNativeDriver
+        backdropColor="rgba(0, 0, 0, 0.5)"
+        backdropOpacity={0.5}
+        backdropTransitionOutTiming={0}
+      >
+        <View className="bg-white rounded-xl p-8 shadow-xl max-w-[100%] mx-auto">
+          <Text className="text-3xl font-bold text-green-600 mb-4 text-center">Your Score</Text>
+          <Text className="text-xl text-gray-600 text-center mb-6">
+            You scored {score} out of {parsedData.length}.
+          </Text>
+        </View>
+      </Modal>
     </ScrollView>
   );
 };
